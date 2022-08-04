208928197f55511545544c9837eea84f1a6a68ef
Faizaan Mahmud <faizaan90@hotmail.com>
2022-08-04 12:54:49 -0500

    Adding simple list card component

---
+++ base_dash_app/components/lists/simple_list/simple_list.py
@@ -0,0 +1,36 @@
+from typing import List
+import dash_bootstrap_components as dbc
+from dash import html
+
+from base_dash_app.virtual_objects.interfaces.listable import Listable
+
+
+def render_list_card(
+        listables: List[Listable],
+        title: str,
+        buttons: List = None
+):
+    if buttons is None:
+        buttons = []
+
+    return dbc.Card(
+        children=[
+            dbc.CardHeader(html.H4(title)),
+            dbc.ListGroup([
+                dbc.ListGroupItem(
+                    children=[
+                        html.Div(listable.get_header()[0], style=listable.get_header()[1]),
+                        html.Div(listable.get_text()[0], style=listable.get_text()[1])
+                    ]
+                )
+                for listable in listables
+            ], flush=True),
+            dbc.CardFooter(
+                dbc.ButtonGroup(buttons) if len(buttons) > 0 else None,
+            )
+        ],
+        style={
+            "width": "30%", "position": "relative", "float": "left",
+            "margin": "20px", "minWidth": "300px"
+        },
+    )
