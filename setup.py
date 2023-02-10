072e37d74cd010cfe8134994ffe83d1ebaa5ed3b
Faizaan Mahmud <faizaan90@hotmail.com>
2023-02-10 16:56:06 -0600

    Bumping to 0.7.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='base_dash_app',
     packages=find_packages(),
-    version='0.7.4',
+    version='0.7.5',
     description='Base Dash Webapp',
     author='fmahmud',
     license='MIT',
