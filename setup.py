f1badbe7e10562ea770ce70c182a443e75d1ead0
Faizaan Mahmud <faizaan90@hotmail.com>
2023-10-10 15:04:22 -0700

    Bumping to v0.10.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='base_dash_app',
     packages=find_packages(),
-    version='0.10.12',
+    version='0.10.13',
     description='Base Dash Webapp',
     author='fmahmud',
     license='MIT',
