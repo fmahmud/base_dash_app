c2056a4f5dbd25c6205e8bb1526e6818177a1956
Faizaan Mahmud <faizaan90@hotmail.com>
2023-11-02 15:02:49 -0500

    Allowing WorkContainers to specify whether to serialize results or not
    issue: #198

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='base_dash_app',
     packages=find_packages(),
-    version='0.10.16',
+    version='0.10.17',
     description='Base Dash Webapp',
     author='fmahmud',
     license='MIT',
